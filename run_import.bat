21babd6c1b1673ea7f37a700e65496935b1acd06
win-laptop <aaronep@live.com>
2026-04-13 06:22:27 -0400

    del local post import

--- run_import.bat
+++ run_import.bat
@@ -15,9 +15,12 @@ python download_dropbox.py
 :: Run the import script (past 24h by default)
 python import_fit.py
 
+:: Delete all .fit files from the download folder
+del /q "C:\Users\aaronep\Downloads\*.fit"
+
 :: Keep window open to see results
-:: echo.
-:: echo Import process finished.
-:: pause
+echo
+echo Import process finished.
+pause
 
 
